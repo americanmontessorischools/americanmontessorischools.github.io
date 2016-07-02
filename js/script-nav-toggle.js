24592bd7f519c0d2c0594bdedfd907312ad5a932
American Montessori Schools <americanmontessorischools@gmail.com>
2016-07-01 23:18:16 -0400

    updates

--- js/script-nav-toggle.js
+++ js/script-nav-toggle.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
 			  $(".navbar-toggle").on("click", function () {
 				    $(this).toggleClass("active");
 			  });
+
+        $(".dropdown").hover(
+            function(){ $(this).addClass('open') },
+            function(){ $(this).removeClass('open') }
+        );
+
 		});
+
+
